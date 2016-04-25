437f09ff4806d35d0b6972dc0ffda6094d54575e
Luis Aguilar <aguilar.copan@gmail.com>
2016-04-24 23:21:46 -0600

    Configuraciones iniciales
    Configuracion inicial de node, bower y gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+/**
+ * Plugins Gulp
+ */
+	var gulp 			= require('gulp');
+	var	gulpUglify 		= require('gulp-uglify');
+	var gulpRename 		= require('gulp-rename');
+
+/**
+ * Exporta en modo distribución
+ */
+gulp.task('dist', function() {
+	gulp.src('./src/FormValidator.js')
+		.pipe(gulp.dest('./dist'));
+	gulp.src('./src/FormValidator.js')
+		.pipe(gulpUglify())
+		.pipe(gulpRename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./dist'));
+});
